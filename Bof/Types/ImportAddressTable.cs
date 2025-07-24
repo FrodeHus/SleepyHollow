4840acf33a0796d7b4d2f1406096614725593124
Frode Hus <frode.hus@outlook.com>
2025-07-24 07:37:57 +0200

    Add ImportAddressTable class for DLL function management
    Introduces the `ImportAddressTable` class in the `SleepyHollow.Bof.Types` namespace. This class manages import addresses for DLL functions, including methods to add and resolve addresses. It allocates memory for the Import Address Table (IAT) and handles function resolution with error checking and optional debug logging.

---
+++ Bof/Types/ImportAddressTable.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SleepyHollow.Bof.Types;
+
+internal class ImportAddressTable
+{
+    private readonly Dictionary<string, IntPtr> _importAddresses = [];
+    private readonly IntPtr _iatAddress;
+    public ImportAddressTable()
+    {
+        _iatAddress = Lib.VirtualAlloc(IntPtr.Zero, (uint)(2 * Environment.SystemPageSize), Lib.MEM_COMMIT, Lib.PAGE_EXECUTE_READWRITE);
+    }
+
+    public IntPtr AddImportAddress(string importName, IntPtr address)
+    {
+        Marshal.WriteInt64(_iatAddress + (_importAddresses.Count * Environment.SystemPageSize), address.ToInt64());
+        _importAddresses[importName] = _iatAddress + (_importAddresses.Count * 8);
+        return _importAddresses[importName];
+    }
+
+    public IntPtr ResolveLibrary(string libraryName, string functionName)
+    {
+        if (!_importAddresses.TryGetValue($"{libraryName}${functionName}", out var address))
+        {
+            var handle = Lib.LoadLibrary(libraryName);
+            address = Lib.GetProcAddress(handle, functionName);
+            if (address == IntPtr.Zero)
+            {
+                throw new InvalidOperationException($"Function '{functionName}' not found in library '{libraryName}'.");
+            }
+            if (RuntimeConfig.IsDebugEnabled)
+                Console.WriteLine($"Resolved function '{functionName}' in DLL '{libraryName}' at address: 0x{address:X}");
+            AddImportAddress($"{libraryName}${functionName}", address);
+        }
+        return address;
+    }
+}
